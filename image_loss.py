f4b45492c9327bbc10a38e0e5b91e8bcf6fab5b6
Masayuki Tanaka <mastnk@gmail.com>
2025-07-03 15:33:33 +0900

    group_roll

--- image_loss.py
+++ image_loss.py
@@ -12,10 +12,16 @@ def _make_perm( nElements, nGroups, roll=1 ):
 
     return perm
 
-def group_roll( x ):
-    pass
+def channel_group_roll( x, nElements, nGroups, roll=1 ):
+    perm = _make_perm( nElements, nGroups )
+    return x[:, perm, :, :]
 
 if( __name__ == "__main__" ):
     perm = _make_perm( 3, 4 )
     print( perm )
+
+    x = torch.tensor( list(range(3*4)), dtype=torch.float ).view( 1, 3*4, 1, 1 )
+    print(x)
+    x = channel_group_roll( x, 3, 4 )
+    print(x)
 
